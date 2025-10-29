1241011c1c394fe83d5b07d5164e50954c8197f8
Joseph Ajayi <ajayimiles73@gmail.com>
2025-10-29 14:12:45 +0100

    add user signup and login functionality to codebase

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Router } from 'express';
 import { config } from './config/env.js';
+import { signupUserSchema } from './validators/user.js';
+import { hashPassword } from './utils/bcrypt.js';
 
 const app = express();
 
@@ -58,24 +60,65 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(config.port, () => {
-    console.log(`server1 running on ${config.port}`);
-});
 
 router.get('/', (req, res) => {
-    res.send("Hello World")
+  res.send("Hello Earth")
 });
 
 router.get('/users', (req, res) => {
-    res.send(users)
+  res.send(users)
 });
 
-router.post('/users/register', (req, res) => {
-    const {id, firstName, lastName, email, password, role, createdAt} = req.body;
-    users.push({id, firstName, lastName, email, password, role, createdAt});
-    return res.send("user registered sucessfully", users);
+router.post('/users/register', async (req, res) => {
+
+  // first thing is to validate the users's input- using joi 
+  const {error, value } = signupUserSchema.validate(req.body);
+
+  // throw an error if a field is missing
+  if(error) return res.status(400).json({error: error.message});
+  
+  // destructure user data into variable "value"
+  const {id, firstName, lastName, email, password, role, createdAt} = value;
+
+  // check if user already exists with the email
+  const user = users.find((user) => user.email === email);
+
+  // throw an error if a user was found with that email
+  if(user) return res.status(400).json({error: "Account already exists"});
+  
+  // hash, or encrypt user's password before storing into DB
+  const hashedPassword = await hashPassword(password);
+  
+  users.push({ id, firstName, lastName, email, password:hashedPassword, role, createdAt});
+  return res.status(201).json({message: "user registered sucessfully", users});
 });
 
-router.get('/products', (req, res) => {
-    res.send("This is the products endpoint")
+router.post('/users/login', async (req, res) => {
+
+  // get data from frontend
+  const {email, password} = req.body;
+
+  // validate user's data manually
+  if(!email || !password) return res.status(400).json({error: "email and password are rquired"});
+
+  // check if user exists
+  const userExists = users.find((user) => user.email === email);
+
+  // throw an error if user is not found
+  if(!userExists) return res.status(404).json({error: "User not found. Kindly create an account to login"});
+
+  // check user's password
+  if(userExists.password !== password) return res.status(400).json({error: "Invalid credentials"});
+
+  // login upon successful validation
+  return res.status(200).json({message: "User logged in successfully!"});
+  
 });
+
+router.get('/products', async (req, res) => {
+  res.send("This is thje product endpoint")
+});
+
+  app.listen(config.port, () => {
+      console.log(`server1 running on ${config.port}`);
+  });
